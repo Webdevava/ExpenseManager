5d2a79d92f94dff6dc6592650ccca7c190468f84
Ankur <ankurauti@gmail.com>
2023-05-15 11:17:39 +0530

    fresh first commit

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
@@ -16,23 +16,14 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   })
 
-  let expensesContent = <p>no expense(s) found</p>
-  if(filteredExpenses.length > 0){
-    expensesContent =filteredExpenses.map((expense) =>( 
-            <ExpenseItem  
-              key={expense.id}
-              title= {expense.title} 
-              amount={expense.amount} 
-              date={expense.date}
-            />
-        ))
-  }
+  
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
